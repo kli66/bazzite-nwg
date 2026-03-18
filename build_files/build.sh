ddabc91b4414021ace122e2817d5d9888c723e74
Kai Li <likaictw@gmail.com>
2026-03-18 20:49:22 +0800

    Install Emacs, ripgrep, and fd-find at build time

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux emacs ripgrep fd-find
 
 # Use a COPR Example:
 #
